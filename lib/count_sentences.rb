2666702c9c1a337ff04bda075f94f003062f3345
Khang <khang.le27@gmail.com>
2019-02-21 13:56:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,9 +15,11 @@ class String
   end
 
   def count_sentences
-    self.split(/[.!?]/).delete_if {|element| element == " " || element == "" || element.nil?}.count
+    #self.split(/[.!?]/).delete_if {|element| element == " " || element == "" || element.nil?}.count
+    self.split(/[.!?] /).count #any of the symbols in [] followed by blank space
+    #self.split(/[.?!]/).reject{|element| element.empty?}
     #binding.pry
   end
 end
 
-#string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+#string = "This, well, is a sentence. This is too!! And so is this, I think? Woo.."
