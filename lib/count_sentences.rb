b0943d4b6cd81ce9402497467aa6b33f6b0ec180
github username <github email address>
2019-02-23 22:17:26 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.end_with?(".")
   end
 
   def question?
-
+   self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
 
+  def count_sentences
+    now = self.split(/[.?!]/)
+    filtered = now.select {|item| item != ""}
+    filtered.count
   end
 end
